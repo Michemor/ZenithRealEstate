55652658d548ac9e4120755a3c55bc5540c97019
Michemor <yatoijema@gmail.com>
2023-12-02 12:06:28 +0300

    Main class build on it

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO ZEITH REAL ESTATE");
+        //LOGIN PROCESS
+        BasicFinished basicroom1 = new BasicFinished();
+        basicroom1.dimensions();
+    }
+}
