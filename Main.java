34f2fd8cf5b8314639c496f78a5d8eadd7d74397
Michemor <yatoijema@gmail.com>
2023-12-02 23:37:41 +0300

    Main class Elegant and Standard room modified

--- Main.java
+++ Main.java
@@ -1,9 +1,55 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("WELCOME TO ZEITH REAL ESTATE");
-        //LOGIN PROCESS
-        BasicFinished basicroom1 = new BasicFinished();
-        basicroom1.dimensions();
+        Scanner in = new Scanner(System.in);
+        double length;
+        double width;
+        int finish_idx;
+          JOptionPane error = new JOptionPane();
+        // USER - LOGIN 
+        System.out.println("WELCOME TO ZEITH REAL ESTATE");       
+
+        // FIRST OPTION : ROOM DIMENSIONS
+        System.out.println("Enter the length and width for each room in your house: ");
+        System.out.print("Lenght >> ");
+        length = in.nextDouble();
+         System.out.print("Width >> ");
+        width = in.nextDouble();
+         // CATCH ERRORS   
+       
+        
+        // SELECT THE TYPE OF FINISH FOR THE HOUSE
+        System.out.println("Enter the type of finish: ");
+        System.out.println("0. BASIC\n1.STANDARD\n2.ELEGANT");
+        finish_idx = in.nextInt();
+
+        if (finish_idx == 0)
+        {
+            //initialize BASIC child class
+        }
+        else if (finish_idx == 1)
+        {
+          // initialize STANDARD CLASS
+        }
+        else if (finish_idx == 2)
+        {
+            ElegantRoom room = new ElegantRoom(length, width, finish_idx);
+             room.compute_area();
+             room.FinishPrice();
+             room.setBalconies();
+             room.setRoomNumber();
+             System.out.println( "Discount: " + room.computeDiscount());
+             System.out.println("Total Price: " + room.getTotalPrice());
+
+        }
+
+    
+       
     }
+   
 }
--- StandardRoom.java
+++ StandardRoom.java
@@ -1,15 +1,25 @@
+import java.util.*;
 abstract public class StandardRoom {
-    private double room_length;
-    private double room_width;
-    private double base_price = 850000;
+    Scanner in = new Scanner(System.in);
+    double room_length;
+    double room_width;
+     double base_price = 850000;
+    double base_area = 132;
+  
+    
+    //private double finishPrice;
 
-    public StandardRoom(double length, double width)
+    public StandardRoom(double length, double width, int i)
     {
         this.room_length = length;
         this.room_width = width;
+       
     }
 
-    abstract public void compute_area();
+    abstract public double compute_area();
+    abstract public double FinishPrice();
+    abstract public int setBalconies();
+    abstract public double computeDiscount();
   
 
 }
